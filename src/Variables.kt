7f018f02db9a157722dc3d6b0d3ccaa9cee7ce53
Monjur3175 <xavieritsolution@gmail.com>
2024-07-31 10:56:30 +0600

    31 July,2024

---
+++ src/Variables.kt
@@ -0,0 +1,12 @@
+
+fun main(args:Array<String>) {
+    val name="Monjur"
+    val age=25
+    val dep: String?
+    dep="ICE"
+    print("Name= "+name+"\n")
+    print("Age= "+age+"\n")
+    print("Department= "+dep+"\n")
+
+}
+
